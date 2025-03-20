2e1ff81b688f1cace6743ef62c9ac11d70efda1b
박민진 <114579651+pmj-chosim@users.noreply.github.com>
2025-03-20 17:12:03 +0900

    Update PpgTrackerListener.kt

--- wear/model/PpgTrackerListener.kt
+++ wear/model/PpgTrackerListener.kt
@@ -15,9 +15,7 @@ import javax.inject.Singleton
 class PpgTrackerListener @Inject constructor() : HealthTracker.TrackerEventListener {
     companion object {
         private const val APP_TAG = "PPG Monitor" // 로그 태그
-        private const val STATUS_NO_CONTACT = -3 // 센서가 피부에서 떨어진 상태를 나타내는 값 //넣어야 하는데 모르겠음
-        private const val STATUS_SIGNAL_WEAK = -8 // PPG 신호가 약한 상태를 나타내는 값 //넣어야 하는데 모르겠음
-        private const val STATUS_SIGNAL_TOO_WEAK = -10 // PPG 신호가 너무 약한 상태를 나타내는 값 //넣어야 하는데 모르겠음
+        private const val STABLE_CONTACT = 0 //정상 연결, PPG 센서 STATUS 값은 -1(비정상) 또는 0(정상)
     }
 
     private val avgPpgGreen = AtomicReference(0f) // GREEN PPG 평균값
@@ -37,11 +35,21 @@ class PpgTrackerListener @Inject constructor() : HealthTracker.TrackerEventListe
     override fun onDataReceived(@NonNull list: List<DataPoint>) {
         if (list.isEmpty()) return
 
-        // 첫 번째 데이터 포인트에서 상태 받음. 데이터 상태가 올바른지 판단해서 isLeadOffValue에 저장.
-        var dump=list[0].getValue(ValueKey.PpgSet.Status)
-        val isLeadOnorOff = (dump != STATUS_NO_CONTACT && dump != STATUS_SIGNAL_WEAK && dump != STATUS_SIGNAL_TOO_WEAK)
-        ppgStatus.set(isLeadOnorOff)
+        // 첫 번째 데이터 포인트에서 상태 받음. PPG 센서 제대로 연결 X(Status가 비정상이면 종료
+        val firstDataPoint = list[0]
+        val isPpgGreenStatus = firstDataPoint.getValue(ValueKey.PpgSet.PPG_GREEN_STATUS) ?: -1
+        val isPpgRedStatus = firstDataPoint.getValue(ValueKey.PpgSet.PPG_RED_STATUS) ?: -1
+        val isPpgIRStatus = firstDataPoint.getValue(ValueKey.PpgSet.PPG_IR_STATUS) ?: -1
+        // 한 센서라도 비정상적으로 연결되어 있는지 확인.
+        val isContactedWrong = (isPpgGreenStatus != STABLE_CONTACT ||
+                isPpgRedStatus != STABLE_CONTACT ||
+                isPpgIRStatus != STABLE_CONTACT)
 
+        // 센서 접촉 상태 업데이트
+        ppgStatus.set(!isContactedWrong)
+
+        // 비정상 상태라면 함수 종료
+        if (isContactedWrong) return
 
 
         // 각 채널의 PPG 값 합계를 저장할 변수들
@@ -49,32 +57,24 @@ class PpgTrackerListener @Inject constructor() : HealthTracker.TrackerEventListe
         var sumRed = 0f
         var sumIr = 0f
 
-        // 각 채널의 상태를 저장할 변수들
-        var greenStatus = 0
-        var redStatus = 0
-        var irStatus = 0
 
         list.forEach { dataPoint ->
-            sumGreen += dataPoint.getValue(ValueKey.PpgSet.PPG_GREEN)
-            sumRed += dataPoint.getValue(ValueKey.PpgSet.PPG_RED)
-            sumIr += dataPoint.getValue(ValueKey.PpgSet.PPG_IR)
-
-            greenStatus = dataPoint.getValue(ValueKey.PpgSet.GREEN_STATUS)
-            redStatus = dataPoint.getValue(ValueKey.PpgSet.RED_STATUS)
-            irStatus = dataPoint.getValue(ValueKey.PpgSet.IR_STATUS)
+            sumGreen += dataPoint.getValue(ValueKey.PpgSet.PPG_GREEN) ?: 0f
+            sumRed += dataPoint.getValue(ValueKey.PpgSet.PPG_RED) ?: 0f
+            sumIr += dataPoint.getValue(ValueKey.PpgSet.PPG_IR) ?: 0f
         }
 
-        // 각 채널의 평균값 계산
+        // 각 채널의 평균값 계산해서 객체에 저장
         avgPpgGreen.set(sumGreen / list.size)
         avgPpgRed.set(sumRed / list.size)
         avgPpgIr.set(sumIr / list.size)
 
 
         // 로그 출력
-        Log.i(APP_TAG, "PPG GREEN mean: ${avgPpgGreen.get()}, Status: $greenStatus")
-        Log.i(APP_TAG, "PPG RED mean: ${avgPpgRed.get()}, Status: $redStatus")
-        Log.i(APP_TAG, "PPG IR mean: ${avgPpgIr.get()}, Status: $irStatus")
-        Log.i(APP_TAG, "Sensor contact status: ${if (ppgStatus.get()) "Off" else "On Contact"}")
+        Log.i(APP_TAG, "PPG GREEN mean: ${avgPpgGreen.get()}, Status: $isPpgGreenStatus")
+        Log.i(APP_TAG, "PPG RED mean: ${avgPpgRed.get()}, Status: $isPpgRedStatus")
+        Log.i(APP_TAG, "PPG IR mean: ${avgPpgIr.get()}, Status: $isPpgIRStatus")
+        Log.i(APP_TAG, "Sensor correct contact status: ${if (ppgStatus.get()) "On Contact" else "Off"}")
     }
 
     /**
@@ -82,7 +82,7 @@ class PpgTrackerListener @Inject constructor() : HealthTracker.TrackerEventListe
      * - 현재는 단순히 로그 출력
      */
     override fun onFlushCompleted() {
-        Log.i(APP_TAG, "onFlushCompleted called of ECG")
+        Log.i(APP_TAG, "onFlushCompleted called")
     }
 
     /**
@@ -116,6 +116,7 @@ class PpgTrackerListener @Inject constructor() : HealthTracker.TrackerEventListe
     /**
      * 현재 센서 접촉 상태를 반환하는 함수
      */
-    fun isLeadOff(): Boolean = leadOff.get()
+    fun isLeadOff(): Boolean = ppgStatus.get()
+
 
 }
